320d178c3c7a627bd34ff7efb0e1fca9cfc33ac3
supremindset <152413705+supremindset@users.noreply.github.com>
2024-12-27 03:35:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var GHPATH = '/PAKAR-PRO';
-var CACHE_NAME = 'cache-v1.08';
+var CACHE_NAME = 'cache-v1.09';
 var urlsToCache = [
   `${GHPATH}/`, // Cache the root path
   `${GHPATH}/index.html`, // Cache your main HTML file
